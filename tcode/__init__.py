3145e178dee1ac0178f4bdb86616e05193f48d75
Erickrus <hyinghao@hotmail.com>
2026-03-21 19:54:37 +0800

    Add files via upload

--- tcode/__init__.py
+++ tcode/__init__.py
@@ -1,7 +1,9 @@
 # tcode package
+__version__ = "0.0.1"
+
 from .event import EventBus, Event
 from .session import SessionManager, Message, Part
 from .storage import Storage
 from .storage_file import FileStorage
 
-__all__ = ["EventBus", "Event", "SessionManager", "Message", "Part", "Storage", "FileStorage"]
+__all__ = ["EventBus", "Event", "SessionManager", "Message", "Part", "Storage", "FileStorage", "__version__"]
